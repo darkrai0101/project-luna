5733d50b4cdb811439f3551b808657619a82a832
darkrai0101 <nv.trung91@gmail.com>
2013-12-03 18:02:15 +0700

    A

--- app.js
+++ app.js
@@ -148,9 +148,162 @@ setInterval(function(){
  // ROUTES
 
 app.get('/', routes.index);
-app.get('/user', user.list);
-app.get('/user/quick-create', user.quickCreate);
-app.get('/user/auth-token/:token', user.authToken);
+// app.get('/user', user.list);
+// app.get('/user/quick-create', user.quickCreate);
+// app.get('/user/auth-token/:token', user.authToken);
+app.all('/user', function(req, res, next){
+  return res.send('test');
+});
+app.all('/user/quick-create', function(req, res, next){
+    var option = req.query;
+  if(!option.email){
+    res.render('404', {url: req.url});
+    return;
+  }
+  // var option = {
+  //  desc:   'test',
+ //     hour:   '2',
+ //     minute: '40',
+ //     period: 'pm',
+ //     date:   '03',
+ //     month:  '12',
+ //     repeat: '1',
+ //     email:  'trungpheng@gmail.com'
+  // };
+
+  var email = option.email;
+  if(option.period === 'pm') hour = parseInt(option.hour) + 12; else hour = option.hour;
+
+  var solarDate;
+
+  var now = new Date();
+  if(option.repeat == 0){
+    //repeat theo ngay
+    var schedule_date = now.getDate();
+    console.log(schedule_date);
+    if(now.getHours() > hour){
+      schedule_date += 1;
+    }
+    solarDate = func.toStringDate(now.getFullYear(), now.getMonth(), schedule_date, hour, option.minute);
+
+  }else if(option.repeat == 1){
+    //repeat theo thang
+    var schedule = amduonglich.getNextSolarDateOfLunarDate(parseInt(option.date));
+    
+    var schedule_date = schedule[0];
+    var schedule_month = schedule[1];
+    var schedule_year = schedule[2];
+
+    solarDate = func.toStringDate(schedule_year, schedule_month, schedule_date, hour, option.minute);
+  }else{
+    //repeat theo nam
+    var schedule = amduonglich.getNextSolarDateOfLunarDateAndMonth(parseInt(option.date), parseInt(option.month));
+    var schedule_date = schedule[0];
+    var schedule_month = schedule[1];
+    var schedule_year = schedule[2];
+
+    solarDate = func.toStringDate(schedule_year, schedule_month, schedule_date, hour, option.minute);
+  };
+  
+  console.log(solarDate);
+
+  db.query('select id from users where email = ? limit 1', email, function(err, rows, fields){
+    if(err) throw err;
+
+    var token = func.createToken();
+    var expire = func.createExpire();
+    var userID;
+
+    if(rows[0]){
+      var row = rows[0];
+      console.log(1);
+      userID = row.id;
+
+    }else{
+      var arr = {
+        name   : email,
+        email  : email,
+        status : 0
+      };
+
+      db.query('insert into users set ?', arr, function(err, rows, fields){
+        if(err) throw err;
+        userID = rows.insertId;
+      });
+    };
+
+    var arr = {
+      solarDate : solarDate, 
+      userID    : userID,
+      message   : option.decs,
+        hour      : hour,
+        minute    : option.minute,
+        date      : option.date,
+        month     : option.month,
+        beforeHour: 0,
+        repeatType: option.repeat,
+        active    : 0
+    };
+    db.query('insert into calendar set ?', arr, function(err, rows, fields){
+      if(err) throw err;
+
+      var calendarID = rows.insertId;
+      var arr = {
+          userID     : userID,
+          calendarID : calendarID,
+          token      : token,
+          expire     : expire
+      };
+      db.query('insert into token set ?', arr, function(err, rows, fields){
+        if(err) throw err;
+        mailer.auth(email, token);
+        return res.send('1');
+      });
+    });
+  });
+});
+app.all('/user/auth-token/:token', function(req, res, next){
+  var token = req.params.token;
+  if(!token){
+    res.render('404', {url: req.url});
+    return;
+  };
+
+  token = encodeURIComponent(token);
+
+  db.query('select * from token where token = ? limit 1', token, function(err, rows, fiels){
+    if(err) throw err;
+    if(rows[0]){
+      row = rows[0];
+      var expire = row['expire'];
+      var calendarID = row['calendarID'];
+      var now = new Date().getTime();
+
+      if(now < expire){
+        var userID = row['userID'];
+        db.query('update calendar set active = 1 where id = ?', calendarID);
+        db.query('delete from token where token = ?', token);
+
+        db.query('select email from users where id = ?', userID, function(err, rows, fields){
+          if(err) throw err;
+          if(rows[0]){
+            mailer.thankReg(rows[0]['email']);
+
+            return res.send('xac thuc thanh cong');
+          }
+        });
+      }else{
+        db.query('delete from calendar  where id = ?',calendarID);
+        db.query('delete from token  where token = ?',token);
+
+        res.send('ma xac thuc het han');
+      }
+    }else{
+      res.render('404', {url: req.url});
+      return;
+    }
+  });
+});
 
 
 app.get('/404', function(req, res, next){
