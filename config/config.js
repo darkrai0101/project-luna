7df3763448d6f0957dd14adda63c886203e705c6
darkrai0101 <nv.trung91@gmail.com>
2014-01-09 14:39:34 +0700

    sap xep ngay

--- config/config.js
+++ config/config.js
@@ -8,43 +8,46 @@ exports.google = {
     clientSecret: 'jY-O0plAyMjWLV7hfQqZkm_m'
 }
 
-// exports.fb = {
-// 	appId: '1384328445159118',
-//     appSecret: 'f49b30ca0c104e6e0288231324019b24'
-// }
-
-// exports.mysql = {
-// 	host : 'localhost',
-// 	port : '3306',
-// 	user : 'root',
-// 	password : 'mininoic',
-// 	database : 'luna'
-// }
-
-// exports.constant = {
-// 	url : 'http://amduonglich.com',
-// 	domain : 'amduonglich.com',
-// }
-
-// exports.fb = {
-// 	appId: '1424702867767076',
-//     appSecret: '4be0da3ceeeb997035caa7cd939ef64b'
-// }
-
 exports.fb = {
-	appId: '581329681915860',
-    appSecret: '894f083dffa47c1ba025327e95ac9cf7'
+	name: 'master',
+	appId: '1384328445159118',
+    appSecret: 'f49b30ca0c104e6e0288231324019b24'
 }
 
 exports.mysql = {
 	host : 'localhost',
 	port : '3306',
 	user : 'root',
-	password : '',
+	password : 'mininoic',
 	database : 'luna'
 }
 
 exports.constant = {
-	url : 'http://localhost:3000',
-	domain : 'localhost:3000',
+	url : 'http://amduonglich.com',
+	domain : 'amduonglich.com',
 }
+
+// exports.fb = {
+//	name : 'dev',
+// 	appId: '1424702867767076',
+//     appSecret: '4be0da3ceeeb997035caa7cd939ef64b'
+// }
+
+// exports.fb = {
+// 	name : 'test',
+// 	appId: '581329681915860',
+//     appSecret: '894f083dffa47c1ba025327e95ac9cf7'
+// }
+
+// exports.mysql = {
+// 	host : 'localhost',
+// 	port : '3306',
+// 	user : 'root',
+// 	password : '',
+// 	database : 'luna'
+// }
+
+// exports.constant = {
+// 	url : 'http://localhost:3000',
+// 	domain : 'localhost:3000',
+// }
